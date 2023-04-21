d761a090dd2eb8216d1c9b56d2d0db4c7e3f5225
Davit <davit.kv8@gmail.com>
2023-04-22 02:02:08 +0400

    celery demo V1

--- app.py
+++ app.py
@@ -2,14 +2,36 @@ import socket
 import uvicorn
 import time
 
+from pytz import timezone
 from celery import Celery
 from fastapi import FastAPI
+
+Tbilisi_timezone = timezone('Asia/Tbilisi')
 
 app = FastAPI()
 
 celery_app = Celery(
     'tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0'
 )
+
+import TBC.tasks
+
+celery_app.conf.timezone = Tbilisi_timezone
+
+celery_app.conf.beat_schedule = {
+
+    'scheduled_task_1': {
+        'task': 'TBC.tasks.update_balance_data',
+        'schedule': 1800
+    },
+
+    'scheduled_task_2': {
+        'task': 'TBC.tasks.get_movements_data',
+        'schedule': 1800
+    },
+
+}
+
 
 if __name__ == "__main__":
     while True:
